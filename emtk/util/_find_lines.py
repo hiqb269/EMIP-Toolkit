f473508084d5643c5df62688390f51ded2b80b22
hiqb269 <hiqb269@aucklanduni.ac.nz>
2025-09-02 23:58:25 +1200

    Refactor line extraction to avoid DataFrame append
    Replaces in DataFrame append in _find_lines because it has been removed from latest version of pandas.

--- emtk/util/_find_lines.py
+++ emtk/util/_find_lines.py
@@ -15,21 +15,17 @@ def _find_lines(aois: pd.DataFrame) -> pd.DataFrame:
     pandas.DataFrame
         Color of the background of the image. "Black" or "white".
     '''
-
-    results = pd.DataFrame({
-        'line_num': pd.Series(dtype='int'),
-        'line_y': pd.Series(dtype='float'),
-        'line_height': pd.Series(dtype='float')})
-
+    temp_rows = []
     for _, row in aois.iterrows():
         name, y, height = row["name"], row["y"], row["height"]
         line_num = re.search('\d+', name).group(0)
-
-        results = results.append({
-            "line_num": int(line_num),
-            "line_y": y + height / 2,
-            "line_height": height,
-        }, ignore_index=True)
-
+        
+        temp_rows.append({
+        "line_num": int(line_num),
+        "line_y": y + height / 2,
+        "line_height": height,
+        })
+    
+    results = pd.DataFrame(temp_rows)
     results = results.drop_duplicates(subset="line_num")
     return results
