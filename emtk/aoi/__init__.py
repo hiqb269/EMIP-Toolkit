1e82db972479ccf82c2b4f37c50229f20a0c0d19
hiqb269 <hiqb269@aucklanduni.ac.nz>
2025-09-04 00:13:12 +1200

    Add add_tokens_to_all_AOIs utility function
    Introduces add_tokens_to_all_AOIs in a new module and updates __init__.py to export it. This function maps image names to code files and extracts tokens for AOIs of kind 'sub-line'

--- emtk/aoi/__init__.py
+++ emtk/aoi/__init__.py
@@ -2,3 +2,4 @@ from .find_aoi import find_aoi
 from .add_tokens_to_AOIs import add_tokens_to_AOIs
 from .add_srcml_to_AOIs import add_srcml_to_AOIs
 from .hit_test import hit_test
+from .add_tokens_to_all_AOI import add_tokens_to_all_AOIs
