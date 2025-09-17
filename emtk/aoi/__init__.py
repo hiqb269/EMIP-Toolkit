ab56d7e31d2194011792ce3d99e90d93fa6458e0
hiqb269 <hiqb269@aucklanduni.ac.nz>
2025-09-17 15:01:32 +1200

    Add line token extraction for AOIs
    Introduced add_line_token_to_AOI.py to extract code lines as tokens for 'line' AOIs. Updated emtk.aoi and emtk.parsers __init__.py files to import new modules. Modified hit_test to accept a gender_col argument for improved data handling. Updated hit test to show gender of the participant.

--- emtk/aoi/__init__.py
+++ emtk/aoi/__init__.py
@@ -3,3 +3,4 @@ from .add_tokens_to_AOIs import add_tokens_to_AOIs
 from .add_srcml_to_AOIs import add_srcml_to_AOIs
 from .hit_test import hit_test
 from .add_tokens_to_all_AOI import add_tokens_to_all_AOIs
+from .add_line_token_to_AOI import add_line_token_to_AOI
